fde79ef041530748614e1adc42dc40bb050d9171
Priyamalka W D N <nimsarapriyamalka@gmail.com>
2026-04-01 17:00:29 +0530

    Initial backend commit

--- server.js
+++ server.js
@@ -34,4 +34,5 @@ mongoose.connect(mongoURI)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on port ${PORT}`);
+    module.exports = app; 
 });
